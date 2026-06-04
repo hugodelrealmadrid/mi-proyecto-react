5ccf8e37f6127009e07a63a04c45ebac0550e315
hugodelrealmadrid <hugodelrealmadrid@gmail.com>
2026-06-04 18:08:25 -0400

    Reapertura del proyecto y ajustes

---
+++ proyecto_web/src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3bzQTb8qiG5-K_8l8KY8upEQOazBXgVQ",
+  authDomain: "proyecto-web-be1ab.firebaseapp.com",
+  projectId: "proyecto-web-be1ab",
+  storageBucket: "proyecto-web-be1ab.firebasestorage.app",
+  appId: "1:1074343850514:web:e498e4d6b8ef39ae205ecb",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ proyecto_web/src/pages/About.jsx
@@ -0,0 +1,5 @@
+function About() {
+  return <h1 className="text-3xl text-red-500">About</h1>;
+}
+
+export default About;
